4500a91cf19d6f831b948cc5862509314ac45bf2
John Hixson <johnhixson2020@u.northwestern.edu>
2019-11-18 18:19:43 -0600

    Completed

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,8 +1,9 @@
-// from data.js
-var tableData = data;
 
 // select tbody 
 var tbody = d3.select("tbody");
+
+// from data.js
+var tableData = data;
 
 // using Object.entries to get key, value data inside of the table
 // and loop through them to add to the table in html
@@ -17,70 +18,24 @@ function buildTable(data){
 
 buildTable(tableData)
 
-//select the web user's input and the filter button
-var dateInputText = d3.select("#datetime")
-var button = d3.select("filter-btn")
-
-// filter data with date that the user inputs
-function clickSelect(){
-    //don't refresh the page!
+// Event that Triggers a Function When the Button is Clicked
+function handleClick(){
+    // Prevents the Page from Refreshing
     d3.event.preventDefault();
-    //print the value that was input
-    console.log(dateInputText.property("value"));
-    //create a new table showing only the filterd data
-    var new_table = tableData.filter(sighting => sighting.datetime===dateInputText.property("value"))
-    //display the new table
-    displayData(new_table);
+    // Select HTML Input Element & Get the Value Property of that Input Element
+    let date = d3.select("#datetime").property("value");
+    let filterData = tableData;
+
+    // Check if a Date was Entered & Filter Data Using that Date;
+    if(date) {
+        // Apply Filter to the Table Data to Only Keep Rows Where datetime Value Matches the Filter Value
+        filterData = filterData.filter((row) => row.datetime === date);
+    }
+    // Build Table with Filtered Data
+    buildTable(filterData);
 }
+// `on` Function to attach an Event to the Handler Function
+d3.selectAll("#filter-btn").on("click", handleClick);
+// Build Table with data.js 
+buildTable(tableData);
 
-// event listener to handle change and click
-dateInputText.on("change", clickSelect)
-
-//data.forEach(function(weatherReport) {
-    //   console.log(weatherReport);
-    // });
-    
-    // // Step 2:  Use d3 to append one table row `tr` for each weather report object
-    // // Don't worry about adding cells or text yet, just try appending the `tr` elements.
-    // data.forEach(function(weatherReport) {
-    //   console.log(weatherReport);
-    //   var row = tbody.append("tr");
-    // });
-    
-    // // Step 3:  Use `Object.entries` to console.log each weather report value
-    // data.forEach(function(weatherReport) {
-    //   console.log(weatherReport);
-    //   var row = tbody.append("tr");
-    
-    //   Object.entries(weatherReport).forEach(function([key, value]) {
-    //     console.log(key, value);
-    //   });
-    // });
-    
-    // // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
-    // data.forEach(function(weatherReport) {
-    //   console.log(weatherReport);
-    //   var row = tbody.append("tr");
-    
-    //   Object.entries(weatherReport).forEach(function([key, value]) {
-    //     console.log(key, value);
-    //     // Append a cell to the row for each value
-    //     // in the weather report object
-    //     var cell = row.append("td");
-    //   });
-    // });
-    
-    // // Step 5: Use d3 to update each cell's text with
-    // // weather report values (weekday, date, high, low)
-    // data.forEach(function(weatherReport) {
-    //   console.log(weatherReport);
-    //   var row = tbody.append("tr");
-    //   Object.entries(weatherReport).forEach(function([key, value]) {
-    //     console.log(key, value);
-    //     // Append a cell to the row for each value
-    //     // in the weather report object
-    //     var cell = row.append("td");
-    //     cell.text(value);
-    //   });
-    // });
-    
